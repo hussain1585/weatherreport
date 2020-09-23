f74020f26c229d57aade3843c05aa462f339cd29
Hussain Akhtar Wahid <hussain@chegg.com>
2020-09-23 16:28:22 +0530

    adding spring boot applicaiton main file

---
+++ src/main/java/com/sapient/sapientweatherreport/SapientweatherreportApplication.java
@@ -0,0 +1,15 @@
+package com.sapient.sapientweatherreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapientweatherreportApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SapientweatherreportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sapient/sapientweatherreport/SapientweatherreportApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sapient.sapientweatherreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapientweatherreportApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
