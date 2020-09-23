1ae1b7dce71498946ae57a7db85a8eb0998ca345
Hussain Akhtar Wahid <hussain@chegg.com>
2020-09-23 16:31:27 +0530

    adding utility class with basic functions

---
+++ src/main/java/com/sapient/sapientweatherreport/util/BaseUtil.java
@@ -0,0 +1,9 @@
+package com.sapient.sapientweatherreport.util;
+
+public class BaseUtil
+{
+    public static Double convertKelvinToCelsius(Double kelvin)
+    {
+        return (kelvin - 273.15);
+    }
+}
